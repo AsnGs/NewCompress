d8bcba5a522cc5987534cd667c97a9729141eec2
AsnGs <yingjieasn@gmail.com>
2024-06-27 07:06:50 +0000

    alpha-0.2

--- structCompress.py
+++ structCompress.py
@@ -1,27 +1,49 @@
 import os
 import csv
+import pickle
 from config import *
 from utils import *
 
-
-def mergeEdges(edgeList, nodeIndex, v):
-    if len(edgeList) == 1 : return edgeList[0]
-    mergedEdge = [nodeIndex, v]  # [merged]
-    times = sorted(list(set([int(e[1]) for edges in edgeList for e in edges])))
-    # startTime, endTime = min(times), max(times)
-    mergedEdge.extend([times[0], times[-1]])  
-
-    specificContent = {}  # {timeOffset: [vertexOffset, type]}  #!  vertexOffset -> vertexIndex better?
+def consAndCodeContent(edgeList):
+    content = {}  # {timeOffset: [vertexOffset, type]} 
 
     for i in range(len(edgeList)):
         for j in range(len(edgeList[i])):
             offsetTime = int(edgeList[i][j][1]) - minsTime #  offset
-            if offsetTime not in specificContent.keys(): specificContent[offsetTime] = []
-            specificContent[offsetTime].append([i, rel2id[edgeList[i][j][0]]])
+            if offsetTime not in content.keys(): content[offsetTime] = []
+            content[offsetTime].append([i, rel2id[edgeList[i][j][0]]])
+    return content
+
+# 本来是将单 u 的节点不合并不映射，放在外面处理了，但是考虑后还是将其统一了，同时也映射，算src 部分的编码了
+def mergeEdges(edgeList, mergedVetexIndex, v):
+    mergedEdge = [mergedVetexIndex, v]  # [merged]
+    times = sorted(list(set([int(e[1]) for edges in edgeList for e in edges])))
+    # startTime, endTime = min(times), max(times)
+    if len(edgeList) == 1:
+        mergedEdge.extend([times[0], times[0]])
+    else:
+        mergedEdge.extend([times[0], times[-1]])  
+
+    specificContent = consAndCodeContent(edgeList)
     mergedEdge.append(specificContent)
     return mergedEdge
 
-
+def getFieldValueIndex_MappingDict(jsonObj, mapDict, mins):
+    for i in range(len(jsonObj)):
+        tempField = list(jsonObj.keys())[i]
+        tempValue = jsonObj[tempField]
+        if tempField == 'starttime':
+            if int(tempValue) <= mins[0]:
+                mins[0] = int(tempValue)
+        if tempField == 'endtime':
+            if int(tempValue) <= mins[1]:
+                mins[1] = int(tempValue)
+        else:
+            if tempField not in mapDict.keys():
+                mapDict[tempField] = {}
+            if tempValue not in mapDict[tempField].keys():
+                mapDict[tempField][tempValue] = len(mapDict[tempField].keys())  # {field: {value: index}}
+    return mapDict
 
 
 
@@ -30,12 +52,11 @@ if __name__ == '__main__':
 
     vertexMap = {} # vertexMap : {v:u}
     edgeMap = {} # edgeMap : {(u, v): e} 
-    newEdges = []
-    newVertex = []
+    newEdges = [] # [[dst, starttime, endtime]]
 
-    mergedIndex2allVertexs = {}  # merged Vertex Index to u (one to many)
-    vertex2mergedIndex = {}  # vertex to merged Vertex Index (one to many)
-    medgeEdgeDict = {}
+    mergedIndex2us = {}  # merged Vertex Index to u (one to many)  {i:[u1, u2, ...]}
+    u2mergedIndex = {}  # vertex to merged Vertex Index (one to many) {u:[i1, i2, ...]}
+    mergedIndex2content = {} # merged Vertex Index to its responding content {i1:conten1, ...]}
 
     with open(os.path.join(csv_dir, edge_csv_file), 'r') as csvfile:
         reader = csv.reader(csvfile)
@@ -50,26 +71,36 @@ if __name__ == '__main__':
             if edge[2:] not in edgeMap[(edge[0], edge[1])]:  # src,dst,type,time 完全相同就按一个算了
                 edgeMap[(edge[0], edge[1])].append(edge[2:])
                 
-    nodeIndex = 0
-    edgeIndex = 0
+    mergedVetexIndex = 0
     for v in vertexMap.keys():
-        if len(vertexMap[v]) == 1:
-            u = vertexMap[v][0]
-            timestamp = edgeMap[(vertexMap[v][0], v)][0][1]
-            edgeType  = edgeMap[(vertexMap[v][0], v)][0][0]
-            newEdges.append([u, v, timestamp, timestamp, {(int(timestamp)-minsTime): [[0, rel2id[edgeType]]]} ])
-            continue
-
         U = vertexMap[v]
-        mergedIndex2allVertexs[nodeIndex] = U
+        mergedIndex2us[mergedVetexIndex] = U
         tmpMergeEdges = [edgeMap[(u, v)] for u in U]
-        mergedEdge = mergeEdges(tmpMergeEdges, nodeIndex, v)
-        newEdges.append(mergedEdge)
+        mergedEdge = mergeEdges(tmpMergeEdges, mergedVetexIndex, v)
+        newEdges.append(mergedEdge[1:-1])  # 仅保留[dst, starttime, endtime]到 newEdges中进行后续编码
         for u in U:
-            if u not in vertex2mergedIndex.keys(): vertex2mergedIndex[u] = []
-            vertex2mergedIndex[u].append(nodeIndex)
-            
-        nodeIndex += 1
+            if u not in u2mergedIndex.keys(): u2mergedIndex[u] = []
+            u2mergedIndex[u].append(mergedVetexIndex)
+        mergedIndex2content[mergedVetexIndex] = mergedEdge[-1]
+        mergedVetexIndex += 1
     
-    print('a')
+    mapDict = {}   # {Field1:{Value: Index}, ...}  # dst, starttime, endtime
+    mins = [sys.maxsize,sys.maxsize]  # minStartTime, minEndTime
+    field = ['dst', 'starttime', 'endtime']
+    data = newEdges
+    for i in range(len(data)):
+        tmpData = data[i]
+        jsonObj = {}
+        for j in range(len(field)):
+            jsonObj[field[j]] = tmpData[j]
+        mapDict = getFieldValueIndex_MappingDict(jsonObj=jsonObj, mapDict=mapDict, mins=mins)   
+    
+    with open('u2mergedIndex.pkl', 'wb') as file:
+        pickle.dump(u2mergedIndex, file)
+    with open('mergedIndex2us.pkl', 'wb') as file:
+        pickle.dump(mergedIndex2us, file)
+    with open('mapDict.pkl', 'wb') as file:
+        pickle.dump(mapDict, file)
+    with open('mergedIndex2content.pkl', 'wb') as file:
+        pickle.dump(mergedIndex2content, file)
 
