26c38dcaa27d1010e76880d3365ce3c0d32442c0
AsnGs <yingjieasn@gmail.com>
2024-06-28 01:54:11 +0000

    alpha-0.1

--- structCompress.py
+++ structCompress.py
@@ -4,14 +4,26 @@ import pickle
 from config import *
 from utils import *
 
-def consAndCodeContent(edgeList):
-    content = {}  # {timeOffset: [vertexOffset, type]} 
+# # return content
+# def consAndCodeContent(edgeList):
+#     content = {} # content: {offsetTime:{typeIndex:[vertexOffset]}}
+#     for i in range(len(edgeList)):
+#         for j in range(len(edgeList[i])):
+#             offsetTime = int(edgeList[i][j][1]) - minsTime #  offset
+#             typeIndex = rel2id[edgeList[i][j][0]]
+#             if offsetTime not in content.keys(): content[offsetTime] = []
+#             content[offsetTime].append([i, typeIndex])
+#     return content
 
+def consAndCodeContent(edgeList):
+    content = {} # content: {offsetTime:{typeIndex:[vertexOffset]}}   # 多重字典嵌套
     for i in range(len(edgeList)):
         for j in range(len(edgeList[i])):
             offsetTime = int(edgeList[i][j][1]) - minsTime #  offset
-            if offsetTime not in content.keys(): content[offsetTime] = []
-            content[offsetTime].append([i, rel2id[edgeList[i][j][0]]])
+            typeIndex = rel2id[edgeList[i][j][0]]
+            if offsetTime not in content.keys(): content[offsetTime] = {}
+            if typeIndex not in content[offsetTime].keys(): content[offsetTime][typeIndex] = []
+            content[offsetTime][typeIndex].append(i)
     return content
 
 # 本来是将单 u 的节点不合并不映射，放在外面处理了，但是考虑后还是将其统一了，同时也映射，算src 部分的编码了
@@ -20,9 +32,9 @@ def mergeEdges(edgeList, mergedVetexIndex, v):
     times = sorted(list(set([int(e[1]) for edges in edgeList for e in edges])))
     # startTime, endTime = min(times), max(times)
     if len(edgeList) == 1:
-        mergedEdge.extend([times[0], times[0]])
+        mergedEdge.extend([times[0]-minsTime, times[0]-minsTime])
     else:
-        mergedEdge.extend([times[0], times[-1]])  
+        mergedEdge.extend([times[0]-minsTime, times[-1]-minsTime])  
 
     specificContent = consAndCodeContent(edgeList)
     mergedEdge.append(specificContent)
@@ -33,11 +45,9 @@ def getFieldValueIndex_MappingDict(jsonObj, mapDict, mins):
         tempField = list(jsonObj.keys())[i]
         tempValue = jsonObj[tempField]
         if tempField == 'starttime':
-            if int(tempValue) <= mins[0]:
-                mins[0] = int(tempValue)
+            continue
         if tempField == 'endtime':
-            if int(tempValue) <= mins[1]:
-                mins[1] = int(tempValue)
+            continue
         else:
             if tempField not in mapDict.keys():
                 mapDict[tempField] = {}
@@ -56,7 +66,7 @@ if __name__ == '__main__':
 
     mergedIndex2us = {}  # merged Vertex Index to u (one to many)  {i:[u1, u2, ...]}
     u2mergedIndex = {}  # vertex to merged Vertex Index (one to many) {u:[i1, i2, ...]}
-    mergedIndex2content = {} # merged Vertex Index to its responding content {i1:conten1, ...]}
+    mergedIndex2content = {} # merged Vertex Index to its responding content {i1:content1, ...]}
 
     with open(os.path.join(csv_dir, edge_csv_file), 'r') as csvfile:
         reader = csv.reader(csvfile)
@@ -77,7 +87,7 @@ if __name__ == '__main__':
         mergedIndex2us[mergedVetexIndex] = U
         tmpMergeEdges = [edgeMap[(u, v)] for u in U]
         mergedEdge = mergeEdges(tmpMergeEdges, mergedVetexIndex, v)
-        newEdges.append(mergedEdge[1:-1])  # 仅保留[dst, starttime, endtime]到 newEdges中进行后续编码
+        newEdges.append(mergedEdge[1:-3])  # 仅保留[dst]到 newEdges中进行后续编码,不保留 dst,starttime, endtiem, content，已经这几个要么已经编码了，要么不需要编码
         for u in U:
             if u not in u2mergedIndex.keys(): u2mergedIndex[u] = []
             u2mergedIndex[u].append(mergedVetexIndex)
@@ -86,7 +96,7 @@ if __name__ == '__main__':
     
     mapDict = {}   # {Field1:{Value: Index}, ...}  # dst, starttime, endtime
     mins = [sys.maxsize,sys.maxsize]  # minStartTime, minEndTime
-    field = ['dst', 'starttime', 'endtime']
+    field = ['dst']
     data = newEdges
     for i in range(len(data)):
         tmpData = data[i]
